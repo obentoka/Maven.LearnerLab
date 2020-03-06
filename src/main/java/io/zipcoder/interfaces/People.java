4930f63c5b68c3c6a95f8c5e67e49128c319e209
vle <obentoka@gmail.com>
2020-03-05 21:18:15 -0500

    Both remove done

--- src/main/java/io/zipcoder/interfaces/People.java
+++ src/main/java/io/zipcoder/interfaces/People.java
@@ -1,4 +1,62 @@
 package io.zipcoder.interfaces;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class People {
+
+    List<Person> people;
+
+    public People(){
+        people = new ArrayList<Person>();
+    }
+
+    public People(Person[] people){
+        this();
+        this.people.addAll(Arrays.asList(people));
+    }
+
+    public Boolean add(Person personToAdd){
+        people.add(personToAdd);
+        return true;
+    }
+
+    public Person findById(Long id){
+        for (Person e : people) {
+            if (e.getId().equals(id))
+                return e;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+    public Boolean contains(Person person){
+        for(Person e : people) {
+            if (e.equals(person))
+                return true;
+        }
+        return false;
+    }
+
+    public Boolean remove(Person person){
+        for(Person e : people) {
+            if (e.equals(person)) {
+                people.remove(e);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Boolean remove(Long id){
+        for(Person e : people) {
+            if (e.getId().equals(id)) {
+                people.remove(e);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
--- src/test/java/io/zipcoder/interfaces/PeopleTest.java
+++ src/test/java/io/zipcoder/interfaces/PeopleTest.java
@@ -1,20 +1,88 @@
 package io.zipcoder.interfaces;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class PeopleTest {
 
-    Instructor testInstructor;
-    Student testStudent;
-    Person test;
+    Person testInstructor;
+    Person testStudent;
+    Person testPerson;
     String expectedName;
-    Long expectedId;
+    People test;
 
     @Before
     public void init(){
-
+        expectedName = "bob";
+        testInstructor = new Instructor(1l, expectedName);
+        testStudent = new Student(2l, expectedName);
+        testPerson = new Person(3l, expectedName);
+        test = new People();
     }
 
+    @Test
+    public void addTest(){
+        test.add(testPerson);
+
+        Person actual = test.findById(3l);
+
+        assertEquals(testPerson, actual);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void addTest2(){
+        Person actual = test.findById(4l);
+    }
+
+    @Test
+    public void containsTest(){
+        test.add(testPerson);
+
+         assertTrue(test.contains(testPerson));
+    }
+
+    @Test
+    public void containsTest2(){
+        test.add(testPerson);
+
+        assertFalse(test.contains(testInstructor));
+    }
+
+    @Test
+    public void removeTest(){
+        test.add(testStudent);
+        test.add(testPerson);
+
+        test.remove(testStudent);
+
+        assertFalse(test.contains(testStudent));
+    }
+
+    @Test
+    public void removeTest2(){
+        test.add(testStudent);
+        test.add(testPerson);
+
+        assertFalse(test.remove(testInstructor));
+    }
+
+    @Test
+    public void removeTest3(){
+        test.add(testStudent);
+        test.add(testPerson);
+
+        test.remove(2l);
+
+        assertFalse(test.contains(testStudent));
+    }
+
+    @Test
+    public void removeTest4(){
+        test.add(testStudent);
+        test.add(testPerson);
+
+        assertFalse(test.remove(1l));
+    }
 }
