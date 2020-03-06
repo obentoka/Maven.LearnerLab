80e3945cfff4c083dd0ecea84721a5e948f46317
vle <obentoka@gmail.com>
2020-03-05 19:54:09 -0500

    started people

---
+++ src/test/java/io/zipcoder/interfaces/PeopleTest.java
@@ -0,0 +1,20 @@
+package io.zipcoder.interfaces;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class PeopleTest {
+
+    Instructor testInstructor;
+    Student testStudent;
+    Person test;
+    String expectedName;
+    Long expectedId;
+
+    @Before
+    public void init(){
+
+    }
+
+}
